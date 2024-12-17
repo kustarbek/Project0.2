00ce4d65004403a76ea39fffd4dd981c7761e050
unknown <ulanuulunursultan36@gmail.com>
2024-12-17 20:28:19 +0600

    architecture fix

--- src/pages/home/ui/block1/Block1.jsx
+++ src/pages/home/ui/block1/Block1.jsx
@@ -1,17 +1,10 @@
-// import { Container } from "../../../../shared/ui";
-
-import { Container } from "@/shared/ui";
-
-// import { Container } from "../../../../shared/ui";
-
-
+import { Container } from "../../../../shared/ui";
 
 export function Block1() {
     return (
         <div>
             <Container>
                 <h1>Block1</h1>
-
             </Container>
         </div>
     )

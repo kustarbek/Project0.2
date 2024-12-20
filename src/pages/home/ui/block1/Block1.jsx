7387aa07fbf3d62b57cad8d14b2ceedd8ae18630
Nursultan Ulan uulu <ulanuulunursultan36@gmail.com>
2024-12-20 12:33:33 +0600

    code formater

--- src/pages/home/ui/block1/Block1.jsx
+++ src/pages/home/ui/block1/Block1.jsx
@@ -1,11 +1,61 @@
-import { Container } from "../../../../shared/ui";
+import { Container } from "../../../../shared/ui"
+import img from "../../../../shared/assets/home-block1/Slider-1.jpg"
+import img3 from "../../../../shared/assets/home-block1/Slider-3.jpg"
+import React, { useRef, useState } from "react"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+import "swiper/css/effect-fade"
+
+// import './styles.css';
+
+// import required modules
+import { Autoplay, EffectFade, Pagination, Navigation } from "swiper/modules"
 
 export function Block1() {
-    return (
-        <div>
-            <Container>
-                <h1>Block1</h1>
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <Container>
+        <h1>Block1</h1>
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, EffectFade, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={img} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={img3} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </Container>
+    </div>
+  )
 }

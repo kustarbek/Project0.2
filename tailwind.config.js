5353f28b77d03bdb80b5f4ef910d9890c919abdc
Nursultan Ulan uulu <ulanuulunursultan36@gmail.com>
2024-12-23 22:03:53 +0600

    finish block1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "slide-bg": "slide-bg 2.5s ease-in-out", // Добавление анимации для фона
+      },
+      keyframes: {
+        "slide-bg": {
+          "0%": { "background-size": "120%" }, // Начальный размер фона
+          "100%": { "background-size": "100%" }, // Конечный размер фона
+        },
+      },
+    },
   },
   plugins: [],
 }

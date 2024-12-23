f3571910cbb1e2e6fd2bf0b40a42d96529eeb47c
Nursultan Ulan uulu <ulanuulunursultan36@gmail.com>
2024-12-23 22:05:36 +0600

    fix tailwond config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      animation: {
-        "slide-bg": "slide-bg 2.5s ease-in-out", // Добавление анимации для фона
-      },
-      keyframes: {
-        "slide-bg": {
-          "0%": { "background-size": "120%" }, // Начальный размер фона
-          "100%": { "background-size": "100%" }, // Конечный размер фона
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
